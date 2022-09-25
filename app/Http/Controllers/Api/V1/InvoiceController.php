fe36845a94505da08cc518badc0e9fb12cd7ef16
millaresluis <millaresluis21@gmail.com>
2022-09-25 18:07:04 +0800

    added resources  for data transform & pagination

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -6,6 +6,8 @@ use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceCollection;
 
 class InvoiceController extends Controller
 {
@@ -17,6 +19,7 @@ class InvoiceController extends Controller
     public function index()
     {
         //
+        return new InvoiceCollection(Invoice::paginate());
     }
 
     /**
@@ -49,6 +52,7 @@ class InvoiceController extends Controller
     public function show(Invoice $invoice)
     {
         //
+        return new InvoiceResource($invoice);
     }
 
     /**
